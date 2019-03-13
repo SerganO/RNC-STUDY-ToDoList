3c664b8012c5115252a6d38d61cfa1fba0d4296b
SerganO <sergeu.ostrovetskiu.2001@gmail.com>
2019-03-13 17:57:44 +0200

    Auth start

--- ToDoList/ToDoList/Controller/LogInViewController.swift
+++ ToDoList/ToDoList/Controller/LogInViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import Firebase
 
 class LogInViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    
+    @IBAction func LogIn(_ sender: Any) {
+        guard
+            let email = emailField.text,
+            let password = passwordField.text,
+            email.count > 0,
+            password.count > 0
+            else {
+                return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { user, error in
+            if let error = error, user == nil {
+                let alert = UIAlertController(title: "Sign In Failed",
+                                              message: error.localizedDescription,
+                                              preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
+    }
+    
+    @IBAction func SignIn(_ sender: Any) {
+        Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { user, error in
+            if error == nil {
+                Auth.auth().signIn(withEmail: self.emailField.text!,
+                                   password: self.passwordField.text!)
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+   
     
-
+    
+    
+    
 }
